aef9df44379c341fde1c5641501aff05bbfe872b
Brandon Shutter <shutterbrandon@gmail.com>
2013-12-11 20:26:10 -0500

    Initial Commit

---
+++ TrackMix/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  TrackMix
+//
+//  Created by Brandon Shutter on 12/11/13.
+//  Copyright (c) 2013 Brandon Shutter. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ TrackMix/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TrackMix
+//
+//  Created by Brandon Shutter on 12/11/13.
+//  Copyright (c) 2013 Brandon Shutter. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
